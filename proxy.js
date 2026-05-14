3f5b688b0871685bae3e48fd2ce9f1120b437e45
antonovcic222 <antonovcic@yandex.ru>
2026-05-14 08:31:31 +0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -52,7 +52,7 @@ app.post('/genapi-query', express.json(), async (req, res) => {
   };
 
   const payload = {
-    model: "text-embedding-3-large",
+    model: "deepseek-v4-flash",
     messages: [systemMessage, ...messages],
     max_tokens: 1500,
     temperature: 0.3,
